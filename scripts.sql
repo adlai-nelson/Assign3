12b2cef0761f1d436733ace6030f9f4fae3c9a7e
adlai-nelson <adlai.nelson123@gmail.com>
2024-02-21 11:48:10 -0500

    Finalized scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -3,18 +3,18 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 
 -- Create PropertyDetails table
 CREATE TABLE PropertyDetails (
-    PropertyID SERIAL PRIMARY KEY,
-    Address VARCHAR(255),
+    PropertyID SERIAL PRIMARY KEY, -- Serial data type: auto-incrimenting integer
+    Address VARCHAR(255), -- Variable Character 
     City VARCHAR(255),
     State VARCHAR(255),
     Country VARCHAR(255),
     ZoningType VARCHAR(255),
     Utility VARCHAR(255),
-    GeoLocation GEOMETRY(Point, 4326), -- Spatial data type
-    CityPopulation INT
+    GeoLocation GEOMETRY(Point, 4326), -- Geometry data type with SRID 4326
+    CityPopulation INT -- Intiger
 );
 
--- Due to the large number of data fields, there were only 8 properies inpu
+-- Due to the large number of data fields, there were only 8 properies input
 -- Data was fabricated for the purposes of this assignment, points were taken in semi-random pattern in respective cities
 INSERT INTO PropertyDetails (Address, City, State, Country, ZoningType, Utility, Geolocation, CityPopulation)VALUES
 ('123 STREET RD', 'WORCESTER', 'MA', 'USA', 'SINGLE FAMILY', 'WATER', ST_GeomFromText('POINT(42.2621462 -71.8353757)', 4326), '205918'),
@@ -30,8 +30,9 @@ INSERT INTO PropertyDetails (Address, City, State, Country, ZoningType, Utility,
 SELECT * FROM PropertyDetails;
 -----------------Normalize to 3NF-----------------
 
+-- Create City Demographics table
 CREATE TABLE CityDemographics (
-    City VARCHAR(255) PRIMARY KEY,
+    City VARCHAR(255) PRIMARY KEY, -- City is primary key
     State VARCHAR(255),
     Country VARCHAR(255),
     CityPopulation INT
@@ -42,16 +43,18 @@ INSERT INTO CityDemographics (City, State, Country, CityPopulation) VALUES
 ('WORCESTER', 'MA', 'USA', 205918),
 ('AUBURN', 'MA', 'USA', 16889)
 
-ALTER TABLE PropertyDetails 
-DROP COLUMN CityPopulation, 
-DROP COLUMN State, 
-DROP COLUMN Country;
+-- Drop columns from PropertyDetails 
+ALTER TABLE PropertyDetails DROP COLUMN CityPopulation, DROP COLUMN State, DROP COLUMN Country;
 
+-- Add Forign key constraint to PropertyDetails
+ALTER TABLE PropertyDetails
+    ADD CONSTRAINT city FOREIGN KEY (City) REFERENCES CityDemographics(City);
+	
 -- View data to confirm 3NF compliance
 SELECT * FROM PropertyDetails;
 SELECT * FROM CityDemographics;
 
------------------Normalize to 3NF-----------------
+-----------------Normalize to 4NF-----------------
 
 -- Create PropertyZoning table with foreign key PropertyID
 CREATE TABLE PropertyZoning (
@@ -70,7 +73,7 @@ INSERT INTO PropertyZoning (PropertyID, ZoningType)
 SELECT PropertyID, ZoningType
 FROM PropertyDetails
 ORDER BY PropertyID;
--- Insert PropertyID and Utility into PropertyZoning table
+-- Insert PropertyID and Utility into PropertyUtilities table
 INSERT INTO PropertyUtilities (PropertyID, Utility)
 SELECT PropertyID, Utility
 FROM PropertyDetails
@@ -87,12 +90,28 @@ SELECT * FROM CityDemographics;
 
 -----------------Perform Spatial Queries-----------------
 
--- Find properties within 2km of Clark University (Coordinates: 42.251389 -71.8241867)
+-- Find properties within 1 mile of Clark University (Coordinates: 42.251389 -71.8241867)
 SELECT Address, City
 FROM PropertyDetails
 WHERE ST_DWithin(
     GeoLocation::geography,  -- Cast to geography so units are in meters not degrees.
     ST_GeomFromText('POINT( 42.251389 -71.8241867)', 4326)::geography,
-    2000 -- 2km radius
+    1609 -- 1 mile radius (1609m = 1 mile)
 );
+
+-- Only 1 property is within a mile of Clark U.
+-- What if we want the distance in meters from Clark U to each property?
+
+-- Create ClarkU table, containing Name and geom variables
+CREATE TABLE ClarkU (Name VARCHAR(255), geom GEOMETRY(Point, 4326));
+-- Insert Name and geom values
+INSERT INTO ClarkU VALUES ('CLARKU', ST_GeomFromText('POINT( 42.251389 -71.8241867)', 4326));
+
+
+-- Calculate distance from ClarkU for each property, and order by distance
+SELECT address, city, ST_Distance(PropertyDetails.GeoLocation::geography,ClarkU.geom::geography) as Dist_ClarkU
+	FROM PropertyDetails, ClarkU
+	ORDER BY Dist_ClarkU;
+
+-- CREDIT: https://stackoverflow.com/questions/60349562/postgis-calculate-distances-from-one-point-to-multiple-points
 
